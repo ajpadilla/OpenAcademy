f841d3d31fdd7650fe2f14d486b8c6c9bf5f99ba
ajpadilla <ajpadilla88@gmail.com>
2013-11-30 00:24:40 -0430

    Agregando campos funcionales Remaining seats

--- openacademy.py
+++ openacademy.py
@@ -13,15 +13,30 @@ class course (osv.osv):
          }
 course()
 
-class session(osv.osv):
+class session(osv.osv):   
   _name="session"
   _description="Session"
+
+  def _get_remaining_seats_percent(self,seats,attendee_list):
+      return seats and ((100.0 * (seats - len(attendee_list)))/ seats) or 0
+
+  def _remaining_seats_percent(self,cr,uid,ids,field,arg,context=None):
+      sessions = self.browse(cr,uid,ids,context=context)
+      result = {}
+      for session in sessions :
+         result[session.id] = self._get_remaining_seats_percent(session.seats, session.attendee_ids)
+      return result
+
   _columns = {
        "name" : fields.char("Session Title", 128, required=True),
        "start_date": fields.date("Start Date"),
        "duration" : fields.float("Duration", digits=(6,2), help="Duration in days"),
        "seats" : fields.integer("Seats number"),
        "attendee_ids": fields.one2many("attendee","session_id","Attendee"),
+       "remaining_seats_percent":fields.function(_remaining_seats_percent,
+						 method=True,type='float',
+                                                 string="Remaining seats" 
+                                                ),
        "instructor_id": fields.many2one("res.partner", "Instructor",
                                         domain=["|",("instructor","=",True),
                                                     ("category_id.name",
